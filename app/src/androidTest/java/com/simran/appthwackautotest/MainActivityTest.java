046982205edaadafe758b06ac84cf648b74a50b0
Simran <simran.fitzgerald@meyouhealth.com>
2015-09-03 16:04:05 -0400

    First commit. Added project

---
+++ app/src/androidTest/java/com/simran/appthwackautotest/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.simran.appthwackautotest;
+
+import android.support.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testButton() {
+        onView(withId(R.id.the_button)).perform(click());
+        onView(withId(R.id.the_text_yo)).check(matches(isDisplayed()));
+    }
+}
