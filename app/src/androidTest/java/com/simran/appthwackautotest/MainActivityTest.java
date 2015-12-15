029b26aff1a552c5aff53caf3d156b0f5e20279c
Simran <simran.fitzgerald@meyouhealth.com>
2015-12-15 16:12:06 -0500

    foo

--- app/src/androidTest/java/com/simran/appthwackautotest/MainActivityTest.java
+++ app/src/androidTest/java/com/simran/appthwackautotest/MainActivityTest.java
@@ -19,5 +19,7 @@ public class MainActivityTest {
     public void testButton() {
         onView(withId(R.id.the_button)).perform(click());
         onView(withId(R.id.the_text_yo)).check(matches(isDisplayed()));
+
+        
     }
 }
