cae2e4eb3b0548811b9012177b89858bb6ebb04f
Simran <simran.fitzgerald@meyouhealth.com>
2015-12-15 20:51:54 -0500

    More sleep time

--- aws_pr_test.py
+++ aws_pr_test.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
 	print "Scheduling run..."
 	run_name = 'test_run_' + now
-	time.sleep(10)
+	time.sleep(15)
 	response = devicefarm.schedule_run(
 		projectArn=PROJECT_ARN, 
 		appArn=app_arn,
